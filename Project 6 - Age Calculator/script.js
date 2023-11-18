461074ae088b4e6d9c56982203dbafd00269b376
Saipavan Veeravalli <shadowpoolvs@gmail.com>
2023-11-18 13:10:35 +0530

    project 6 fix

--- Project 6 - Age Calculator/script.js
+++ Project 6 - Age Calculator/script.js
@@ -14,19 +14,18 @@ function calculate() {
     let m2 = today.getMonth()  + 1;
     let y2 = today.getFullYear();
 
-    let d3, m3, y3;
+    let d3 = 0, m3 = 0, y3 = 0;
 
     y3 = y2-y1;
     if(m2 >= m1){
         m3 = m2-m1;
     } else{
-        y--;
+        y3--;
         m3 = 12 + m2 - m1;
     }
-    if(d3 >= d1){
+    if(d2 >= d1){
         d3 = d2 - d1;
     } else{
-        console.log(calDays(m2, y2));
         m3--;
         d3 = calDays(m2, y2) + d2 - d1;
     }
@@ -39,9 +38,10 @@ function calculate() {
 }
 
 const calDays = (month, year) =>{
-    return new Date(year, month, 0).getDate();
+    let date = new Date(year, month, 0).getDate();
+    console.log(date);
+    return date;
 }
 
 
 
-calculate()
